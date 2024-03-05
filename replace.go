545fcd00bba292a32e6324390c20c102fe51aa07
Ivan D <dyakonov0904@gmail.com>
2024-03-05 16:21:21 +0300

    Проверка ветки

--- replace.go
+++ replace.go
@@ -104,9 +104,6 @@ func translate_for_to_go(code string) string {
 	//set
 	code = replaceAllStringRegexp(code, `(?i)\bset\s+([^,]+(?:\{[^}]+\})?),([^)]+)\s*\)?`, "SET($1, $2)\n")
 
-	//dost
-	code = replaceAllStringRegexp(code, `(?i)dost`, "DOST")
-
 	//sleep
 	code = replaceAllStringRegexp(code, `sleep\(([^)]+)\)`, `time.Sleep(($1) * time.Second)`)
 
